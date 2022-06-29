e4c59b7e339a0320105430be17bdb617356cdddb
J1NU2 <sdrtst@naver.com>
2022-06-29 18:02:31 +0900

    추가 - 이미지 업로드 기능

--- upload/models.py
+++ upload/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# 이미지 모델
+class Image(models.Model):
+    image_one = models.ImageField("첫번째 이미지", upload_to="img/upload_img/%Y%m%d",
+    width_field=None, height_field=None, max_length=150)
+
+    image_two = models.ImageField("두번째 이미지", upload_to="img/upload_img/%Y%m%d",
+    width_field=None, height_field=None, max_length=150)
+
+    image_result = models.ImageField("결과 이미지", upload_to="img/upload_img/%Y%m%d",
+    width_field=None, height_field=None, max_length=150)
