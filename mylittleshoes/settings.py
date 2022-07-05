080908841c66eba89a10845680a4181273dbe24e
mankic <miic0803@gmail.com>
2022-07-05 17:07:43 +0900

    수정 - cors 수정

--- mylittleshoes/settings.py
+++ mylittleshoes/settings.py
@@ -43,9 +43,11 @@ INSTALLED_APPS = [
     'upload',
     'rest_framework',
     'recommend',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -103,6 +105,12 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
+]
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
 ]
 
 
