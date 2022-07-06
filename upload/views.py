c13591158515c0ece14190d953593131e7b4a071
DongwooKim <kimphysicsman@gmail.com>
2022-07-06 20:17:21 +0900

    코드 리팩토링

--- upload/views.py
+++ upload/views.py
@@ -1,5 +1,4 @@
 import json
-from django.db.models.query_utils import Q
 
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -7,17 +6,14 @@ from rest_framework import status
 
 from upload.models import Image as ImageModel
 
-from upload.serializers import ImageSerializer
 import os
 from PIL import Image
 
 from io import BytesIO
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.core.files.base import ContentFile
-from mylittleshoes import settings
 
 from history.serializers import HistorySerializer
-from history.models import History as HistoryModel
 from user.models import User as UserModel
 
 IMAGE_WIDTH = 100
@@ -38,9 +34,6 @@ def resize_image(image_field, width=IMAGE_WIDTH, height=IMAGE_HEIGHT, name=None)
 
 # 이미지 등록 페이지
 class ImageUploadView(APIView):
-    # 이미지 등록 페이지 조회
-    def get(self, request):
-        return Response({})
 
     # 새로운 이미지 만들기
     # 이미지 두장 받아서 새로운 스타일의 이미지 만들어서 반환
@@ -68,16 +61,6 @@ class ImageUploadView(APIView):
 
 # 이미지 결과 페이지
 class ImageResultView(APIView):
-    # 이미지 3장 불러오기
-    def get(self, request):
-        # GET 요청으로 image에 대한 정보들 불러오기?
-        image_one = request.GET.get("image_one", "")
-        image_two = request.GET.get("image_two", "")
-        image_result = request.GET.get("image_result", "")
-
-        image_serializer = ImageSerializer(image_one=image_one, image_two=image_two, image_result=image_result)
-
-        return Response(image_serializer.data, status=status.HTTP_200_OK)
 
     # 저장하기 버튼 클릭 : 이미지 저장
     def post(self, request):
